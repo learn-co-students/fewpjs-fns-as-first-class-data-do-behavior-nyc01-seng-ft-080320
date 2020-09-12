c30cef2d984d14552676639663259377b77972f0
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-12 19:10:52 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+
+    const timeSplit = time.split(":").join("")
+    const parsedTime = parseInt(timeSplit)
+
+    if (parsedTime < 1200){
+        return "Good Morning"
+    } else if (parsedTime < 1700 && parsedTime > 1200){
+        return "Good Afternoon"
+    } else {
+        return "Good Evening"
+    }
+}
+
+function displayMessage(string){
+    const greeting = document.getElementById('greeting')
+    greeting.innerText = string
+}
 /* Write your implementation of displayMessage() */
