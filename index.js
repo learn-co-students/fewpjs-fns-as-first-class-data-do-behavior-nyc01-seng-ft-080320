75cc7049b883680f6643279415951860bd630dc3
anickel101 <anson.nickel@gmail.com>
2020-09-13 09:12:30 -0400

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time_string) {
+  let hour = parseInt(time_string.split(':')[0])
+  let min = parseInt(time_string.split(':')[1])
+  let tot_time = hour + min/60
+
+  if (tot_time < 12) {
+    return "Good Morning"
+  } else if (tot_time >= 12 && tot_time <= 17) {
+    return "Good Afternoon"
+  } else if (tot_time > 17) {
+    return "Good Evening"
+  } else {
+    return "Please enter time in HH:MM format"
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string
+}
